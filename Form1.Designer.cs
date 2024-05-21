cead97f839ccee97ce0f8af45a2fcbfbf7a0eef9
Douglas Morales <DouGMoralesVelasquez@hotmail.com>
2024-05-21 09:47:45 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ProyectoAlgebraLineal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.validarButton = new System.Windows.Forms.Button();
+            this.carnetTextBox = new System.Windows.Forms.TextBox();
+            this.carnetModificadoLabel = new System.Windows.Forms.Label();
+            this.LogopictureBox = new System.Windows.Forms.PictureBox();
+            this.qrCodePictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LogopictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qrCodePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese Carné:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Carné modificado:";
+            // 
+            // validarButton
+            // 
+            this.validarButton.Location = new System.Drawing.Point(310, 159);
+            this.validarButton.Name = "validarButton";
+            this.validarButton.Size = new System.Drawing.Size(84, 32);
+            this.validarButton.TabIndex = 2;
+            this.validarButton.Text = "Validar";
+            this.validarButton.UseVisualStyleBackColor = true;
+            this.validarButton.Click += new System.EventHandler(this.validarButton_Click);
+            // 
+            // carnetTextBox
+            // 
+            this.carnetTextBox.Location = new System.Drawing.Point(290, 113);
+            this.carnetTextBox.Name = "carnetTextBox";
+            this.carnetTextBox.Size = new System.Drawing.Size(127, 22);
+            this.carnetTextBox.TabIndex = 3;
+            // 
+            // carnetModificadoLabel
+            // 
+            this.carnetModificadoLabel.AutoSize = true;
+            this.carnetModificadoLabel.Location = new System.Drawing.Point(307, 221);
+            this.carnetModificadoLabel.Name = "carnetModificadoLabel";
+            this.carnetModificadoLabel.Size = new System.Drawing.Size(0, 16);
+            this.carnetModificadoLabel.TabIndex = 4;
+            // 
+            // LogopictureBox
+            // 
+            this.LogopictureBox.Image = global::ProyectoAlgebraLineal.Properties.Resources.Logo_Meso;
+            this.LogopictureBox.Location = new System.Drawing.Point(26, 12);
+            this.LogopictureBox.Name = "LogopictureBox";
+            this.LogopictureBox.Size = new System.Drawing.Size(125, 120);
+            this.LogopictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LogopictureBox.TabIndex = 5;
+            this.LogopictureBox.TabStop = false;
+            // 
+            // qrCodePictureBox
+            // 
+            this.qrCodePictureBox.Location = new System.Drawing.Point(455, 113);
+            this.qrCodePictureBox.Name = "qrCodePictureBox";
+            this.qrCodePictureBox.Size = new System.Drawing.Size(132, 115);
+            this.qrCodePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.qrCodePictureBox.TabIndex = 6;
+            this.qrCodePictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 325);
+            this.Controls.Add(this.qrCodePictureBox);
+            this.Controls.Add(this.LogopictureBox);
+            this.Controls.Add(this.carnetModificadoLabel);
+            this.Controls.Add(this.carnetTextBox);
+            this.Controls.Add(this.validarButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.LogopictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qrCodePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button validarButton;
+        private System.Windows.Forms.TextBox carnetTextBox;
+        private System.Windows.Forms.Label carnetModificadoLabel;
+        private System.Windows.Forms.PictureBox LogopictureBox;
+        private System.Windows.Forms.PictureBox qrCodePictureBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace ProyectoAlgebraLineal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void validarButton_Click(object sender, EventArgs e)
+        {
+            string carnet = carnetTextBox.Text.Trim();
+
+            // Verificar si el carnet tiene 9 dígitos
+            if (carnet.Length != 9 || !EsNumero(carnet))
+            {
+                MessageBox.Show("El carnet ingresado no es válido. Debe tener 9 dígitos numéricos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Verificar si los primeros 4 dígitos están en el rango permitido
+            int anio;
+            if (!int.TryParse(carnet.Substring(0, 4), out anio) || anio < 2016 || anio > 2024)
+            {
+                MessageBox.Show("El año en el carnet ingresado no es válido. Debe estar entre 2016 y 2024.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Generar número de seguridad usando el algoritmo de ISBN-10
+            int numSeguridad= GenerarNumSeguridad(carnet);
+
+            // Concatenar el número de seguridad al carnet
+            string carnetModificado = carnet + numSeguridad;
+
+            // Mostrar el carnet modificado en el label
+            carnetModificadoLabel.Text = carnetModificado;
+
+            // Generar código QR
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(carnetModificado, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            // Mostrar código QR
+            qrCodePictureBox.Image = qrCodeImage;
+        }
+
+        private bool EsNumero(string cadena)
+        {
+            foreach (char c in cadena)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private int GenerarNumSeguridad(string carnet)
+        {
+            // Se asume que el carnet es válido y tiene 9 dígitos
+            int suma = 0;
+            for (int i = 0; i < carnet.Length; i++)
+            {
+                int digito = int.Parse(carnet[i].ToString());
+                suma += digito * (i + 1); // Ponderación incremental
+            }
+            return suma % 10;
+        }
+    }
+}
